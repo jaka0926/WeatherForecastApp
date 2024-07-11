034c12e6f568f23f7b10e8e83398b742c25052cb
Jaka <jaka0926@naver.com>
2024-07-11 18:01:17 +0900

    ScrollView 추가

--- WeatherForecast/MainPage/MainViewController.swift
+++ WeatherForecast/MainPage/MainViewController.swift
@@ -10,13 +10,19 @@ import SnapKit
 
 class MainViewController: BaseViewController {
 
+    let scrollView = UIScrollView()
+    let contentView = UIView()
     let regionName = UILabel()
     let currentTemp = UILabel()
     let todayView = UIView()
+    let todayScrollView = UIScrollView()
+    let todayContentView = UIView()
     let weekView = UIView()
+    let weekScrollView = UIScrollView()
+    let tabBarView = UIView()
     let todayViewTitle = UILabel()
     let weekViewTitle = UILabel()
-    let todayTableView = UITableView()
+    //let todayTableView = UITableView()
     let weekTableView = UITableView()
     let mapButton = UIButton()
     let searchButton = UIButton()
@@ -24,24 +30,35 @@ class MainViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
     override func configureHierarchy() {
-        view.addSubview(regionName)
-        view.addSubview(currentTemp)
-        view.addSubview(todayView)
-        view.addSubview(weekView)
-        todayView.addSubview(todayViewTitle)
-        todayView.addSubview(todayTableView)
-        weekView.addSubview(weekViewTitle)
-        weekView.addSubview(weekTableView)
-        view.addSubview(mapButton)
-        view.addSubview(searchButton)
+        view.addSubview(scrollView)
+            scrollView.addSubview(contentView)
+                contentView.addSubview(regionName)
+                contentView.addSubview(currentTemp)
+                contentView.addSubview(todayView)
+                    todayView.addSubview(todayViewTitle)
+                    todayView.addSubview(todayScrollView)
+                    todayScrollView.addSubview(todayContentView)
+                contentView.addSubview(weekView)
+                    weekView.addSubview(weekViewTitle)
+                    weekView.addSubview(weekTableView)
+        view.addSubview(tabBarView)
+            tabBarView.addSubview(mapButton)
+            tabBarView.addSubview(searchButton)
     }
     override func configureLayout() {
+        scrollView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view).offset(-60)
+        }
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
         regionName.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.top.equalToSuperview().offset(50)
         }
         currentTemp.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -49,20 +66,24 @@ class MainViewController: BaseViewController {
         }
         todayView.snp.makeConstraints { make in
             make.top.equalTo(currentTemp.snp.bottom).offset(50)
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(10)
             make.height.equalTo(200)
         }
         weekView.snp.makeConstraints { make in
             make.top.equalTo(todayView.snp.bottom).offset(20)
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
-            make.height.equalTo(250)
+            make.height.equalTo(550)
+            make.horizontalEdges.bottom.equalTo(contentView.safeAreaLayoutGuide).inset(10)
         }
         todayViewTitle.snp.makeConstraints { make in
             make.top.left.equalToSuperview().inset(10)
         }
-        todayTableView.snp.makeConstraints { make in
+        todayScrollView.snp.makeConstraints { make in
             make.top.equalTo(todayViewTitle.snp.bottom).offset(10)
             make.horizontalEdges.bottom.equalToSuperview()
+        }
+        todayContentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.size.equalToSuperview()
         }
         weekViewTitle.snp.makeConstraints { make in
             make.top.left.equalToSuperview().inset(10)
@@ -71,39 +92,41 @@ class MainViewController: BaseViewController {
             make.top.equalTo(weekViewTitle.snp.bottom).offset(10)
             make.horizontalEdges.bottom.equalToSuperview()
         }
+        tabBarView.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.bottom)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
         mapButton.snp.makeConstraints { make in
-            make.left.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.left.top.equalToSuperview().inset(20)
         }
         searchButton.snp.makeConstraints { make in
-            make.right.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.right.top.equalToSuperview().inset(20)
         }
     }
     override func configureUI() {
-        view.backgroundColor = .darkGray
+        contentView.backgroundColor = .systemTeal
+        tabBarView.backgroundColor = .systemOrange
+        
         regionName.textColor = .white
         regionName.text = "Seoul"
         regionName.font = .boldSystemFont(ofSize: 40)
         
-        currentTemp.textColor = .white
         currentTemp.text = "23.4°"
         currentTemp.font = .boldSystemFont(ofSize: 80)
         
         todayView.backgroundColor = .brown
         todayViewTitle.backgroundColor = .systemTeal
-        todayViewTitle.textColor = .white
         todayViewTitle.text = "3시간 간격의 일기예보"
-        todayTableView.backgroundColor = .lightGray
+        todayScrollView.backgroundColor = .systemRed
+        todayContentView.backgroundColor = .systemGreen
         
         weekView.backgroundColor = .brown
         weekViewTitle.backgroundColor = .systemTeal
-        weekViewTitle.textColor = .white
         weekViewTitle.text = "5일 간의 일기예보"
         weekTableView.backgroundColor = .lightGray
         
         mapButton.setImage(UIImage(systemName: "map"), for: .normal)
-        mapButton.tintColor = .white
         searchButton.setImage(UIImage(systemName: "list.bullet"), for: .normal)
-        searchButton.tintColor = .white
     }
 }
 
