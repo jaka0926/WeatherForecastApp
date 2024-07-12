ecff5e7fb1275601d1edc4fe0f8eac0a4743a0fc
Jaka <jaka0926@naver.com>
2024-07-12 23:50:16 +0900

    WeekTableView 구현

---
+++ WeatherForecast/MainPage/WeekTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeekTableViewCell.swift
+//  WeatherForecast
+//
+//  Created by Jaka on 2024-07-12.
+//
+
+import UIKit
+
+class WeekTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
